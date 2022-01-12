1820102bea4c3ecee0cd747b5cde1ab5233eebf0
shekarvalluri9 <valluri.shekar69@gmail.com>
2022-01-12 17:47:38 +1030

    changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,9 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
+var chars = document.getElementById("chars");
 // Write password to the #password input
+function generatepassword(){
+  console.log("generatepassword")
+  var chars ="!@#$%%&abcdefghijklmnopqrstuvwxyz";
+  var passwordlength="8";
+  var password ="";
+for (var i=0; i<generatepassword.length; i++){
+password = math.floor(math.random()*chars.length)
+}
+return;
+}
 function writePassword() {
-  var password = generatePassword();
+  document.getElementById("display").generatepassword = password;
+function writePassword() {
+  var password = generatepassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
@@ -11,4 +23,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+}
